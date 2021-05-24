7680a1d9c7ecdb4f4305378a0ab857502fbe64ba
ElizavetaTitova <75878468+ElizavetaTitova@users.noreply.github.com>
2021-05-24 21:56:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h
-
 int prioritet (char value) {
     switch (value) {
       case '(': return 0;
@@ -22,40 +21,39 @@ int prioritet (char value) {
 
 std::string infx2pstfx(std::string inf) {
  Tstack <char> Stack1;
-int prioritet;
+    int prioritet;
 char arr;
 char begin = 0;
 std::string total;
 for (int i = 0; i < inf.length(); i++) {
  arr = inf[i];
- prioritet = sequence(arr);
-   if (prioritet > -1) {
-            if ((prioritet == 0 || prioritet > sequence(begin) || Stack1.isEmpty()) && arr != ')') {
+    prioritet = sequence(arr);
+    if (prioritet > -1) {
+        if ((prioritet == 0 || prioritet > sequence(begin) || Stack1.isEmpty()) && arr != ')') {
                 if (Stack1.isEmpty())
  begin = arr;
- Stack1.push(arr);
-            }
-            else if (arr == ')') {
+            Stack1.push(arr);
+        } else if (arr == ')') {
                 while (Stack1.get() != '(') {
  total.push_back(Stack1.get());
  total.push_back(' ');
- Stack1.pop();
+                    Stack1.pop();
                 }
- Stack1.pop();
+            Stack1.pop();
                 if (Stack1.isEmpty())
- begin = 0;
+                    begin = 0;
             }
-            else {
+        else {
                 while (!Stack1.isEmpty() && sequence(Stack1.get()) >= prioritet) {
  total.push_back(Stack1.get());
  total.push_back(' ');
- Stack1.pop();
+                    Stack1.pop();
                 }
-                if (Stack1.isEmpty())
+            if (Stack1.isEmpty())
  begin = inf[i];
- Stack1.push(inf[i]);
-            }
+            Stack1.push(inf[i]);
         }
+    }
         else {
             total.push_back(arr);
             total.push_back(' ');
@@ -64,9 +62,9 @@ for (int i = 0; i < inf.length(); i++) {
     while (!Stack1.isEmpty()) {
  total.push_back(Stack1.get());
  total.push_back(' ');
- Stack1.pop();
-  }
- total.erase(total.end() - 1, total.end());
+        Stack1.pop();
+    }
+    total.erase(total.end() - 1, total.end());
   return total;
 }
 
@@ -81,10 +79,10 @@ int eval(std::string pst) {
       int val2 = Stack2.get();
  Stack2.pop();
       int val1 = Stack2.get();
- Stack2.pop();
+        Stack2.pop();
   switch(pst[i]) {
-        case '*': Stack2.push(val1 * val2);
-            break;
+      case '*': Stack2.push(val1 * val2);
+          break;
         case '/': Stack2.push(val1 / val2);
             break;
         case '+': Stack2.push(val1 + val2);
